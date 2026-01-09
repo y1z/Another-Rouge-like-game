d7138fe683ddddc23e349cab24edf6d5e21d259e
Yhaliff <whyis2cool@gmail.com>
2026-01-09 01:04:35 -0600

    add script for debugging.

---
+++ Scripts/Debug Only/Debug.gd
@@ -0,0 +1,19 @@
+extends Node
+
+## class_name Debug
+
+
+static func d_print(...args: Array) -> void:
+	if !OS.is_debug_build(): return;
+	print(args)
+	pass
+
+
+static func d_print_rich(... args: Array) -> void:
+	if !OS.is_debug_build(): return ;
+	print_rich(args)
+
+
+static func d_printerr(... args: Array) -> void:
+	if !OS.is_debug_build(): return ;
+	printerr(args)
